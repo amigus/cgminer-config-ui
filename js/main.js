32d8aa69a3a9d6327e1ef3fdae6c3105f90342ba
Adam C. Migus <adam@migus.org>
2014-02-16 00:21:03 -0500

    Add a GPUs slider
    - Add an input range (1-4) for 'gpus'
    - Add gpusChanged() that expands the values of each
      text field to reflect the selected number of gpus
    - Add some CSS to style the input element

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ angular.module('cgminerConfigUI',[]).controller('cgminerConfigCtrl', function ($
 		reader.onloadend = function(evt) {
 			if (evt.target.readyState == FileReader.DONE) {
 				$scope.cgminerConfig = JSON.parse(evt.target.result);
+				$scope.gpus = $scope.cgminerConfig['intensity'].match(/,/)
+						.length + 1;
 				$scope.prePopulated = true;
 				$scope.$apply();
 			}
@@ -20,11 +22,10 @@ angular.module('cgminerConfigUI',[]).controller('cgminerConfigCtrl', function ($
 		reader.readAsBinaryString(evt.dataTransfer.files[0]);
 	}, false);
 
-	($scope.addPool = function() {
-		if (null == $scope.cgminerConfig) {
-			$scope.cgminerConfig = { pools: [] };
-		}
+	$scope.cgminerConfig = { pools: [] };
+	$scope.gpus = 1;
 
+	($scope.addPool = function() {
 		var pools = $scope.cgminerConfig.pools;
 
 		$scope.cgminerConfig.pools = [];
@@ -33,7 +34,32 @@ angular.module('cgminerConfigUI',[]).controller('cgminerConfigCtrl', function ($
 				$scope.cgminerConfig.pools.push(pools[i]);
 			}
 		}
-		$scope.cgminerConfig.pools.push({ "url" : "", "user" : "", "pass" : ""});
+		$scope.cgminerConfig.pools.push({
+			'url' : '', 'user' : '', 'pass' : ''
+		});
+	})();
+
+	($scope.gpusChanged = function() {
+		function adjust(setting) {
+			var value = $scope.cgminerConfig[setting];
+			var gpus = $scope.gpus;
+			var result = ("" + $scope.cgminerConfig[setting]).split(",");
+
+			if (result.length > gpus) {
+				result = result.slice(0, gpus);
+			} else {
+				for (var i = 0; i < gpus - result.length; i++) {
+					result.push(result[result.length - 1]);
+				}
+			}
+			$scope.cgminerConfig[setting] = result.join(',');
+		}
+		for (var setting in $scope.cgminerConfig) {
+			if ('auto-fan' != setting && 'auto-gpu' != setting
+					&& 'pools' != setting && 'scrypt' != setting) {
+				adjust(setting);
+			}
+		}
 	})();
 
 	$scope.save = function() {
